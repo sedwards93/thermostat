70b13d46e9744e72247565ab69109af807600a24
Symion Edwards <symion.edwards@gmail.com>
2020-12-03 19:20:37 +0000

    Started to create a UI for the thermostat using jQuery

--- spec/unitSpec.js
+++ spec/unitSpec.js
@@ -11,15 +11,79 @@ describe('Thermostat', function(){
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 
-  it('Can increase the temperature with an up function', function() {
-    thermostat.up()
-    expect(thermostat.getCurrentTemperature()).toEqual(21)
-  });
+  describe('Up', () => {
+    it('Can increase the temperature with an up function', function() {
+      thermostat.up();
+      expect(thermostat.getCurrentTemperature()).toEqual(21)
+    });
+    it('Throws an error if user tries to increase above maximum temperature', () => {
+      for (var i = 1; i < 6; i++) {
+        thermostat.up()
+      };
+      expect( function(){ thermostat.up(); } ).toThrow(new Error('Cannot get any hotter than this!'));
+    })
+  })
+
 
   it('Can decrease the temperature with a down function', function() {
-    thermostat.down()
+    thermostat.down();
     expect(thermostat.getCurrentTemperature()).toEqual(19);
   });
 
+  it('Has a minimum temperature of 10', function() {
+    for (var i = 20; i > 10; i--) thermostat.down();
+    expect( function(){ thermostat.down(); } ).toThrow(new Error('Cannot get any colder than this!'));
+  })
+
+  describe('togglePowerSavingOff', () =>  {
+    it('Can switch between powerSaving to false', function(){
+      thermostat.togglePowerSavingOff();
+      expect(thermostat.powerSaving).toEqual(false);
+    });
+  
+    it('Can switch between powerSaving to true', function(){
+      thermostat.togglePowerSavingOff();
+      thermostat.togglePowerSavingOn();
+      expect(thermostat.powerSaving).toEqual(true);
+    });
+
+    it('Calls setMaxTemperature', function() {
+      spyOn(thermostat, 'setMaxTemperature');
+      thermostat.togglePowerSavingOff();
+      expect(thermostat.setMaxTemperature).toHaveBeenCalled();
+    });
+  });
+
+  describe('resetTemperature', () => {
+    it('Calling resetTemperature will reset the temperature to 20 degrees', () => {
+      thermostat.down();
+      thermostat.down();
+      thermostat.resetTemperature();
+      expect(thermostat.getCurrentTemperature()).toEqual(20)
+    });
+  });
+
+  describe('energyUsage', () => {
+
+    it('Returns low-usage when the temperature is less than 18 degrees', () => {
+      for (var i = 1; i < 5; i++) {
+        thermostat.down();
+      }
+      expect(thermostat.energyUsage()).toEqual('low-usage');
+    });
+    
+    it('Returns medium-usage when the temperature is between 18 and 25 degrees', () => {
+      expect(thermostat.energyUsage()).toEqual('medium-usage');
+    });
+
+    it('Returns high-usage when the temperature is less than 18 degrees', () => {
+      thermostat.togglePowerSavingOff();
+      for (var i = 1; i < 7; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.energyUsage()).toEqual('high-usage');
+    });
+
+  })
 
 })
--- src/thermostat.js
+++ src/thermostat.js
@@ -3,19 +3,69 @@
 class Thermostat {
 
   constructor(){
-    this.temperature = 20
+    this.MINIMUM_TEMPERATURE = 10;
+    this.POWERSAVING_MAX_TEMPERATURE = 25;
+    this.MAX_TEMPERATURE = 32;
+    this.DEFAULT_TEMPERATURE = 20;
+    this.LOW_ENERGY_USAGE = 17;
+    this.MEDIUM_ENERGY_USAGE = 25;
+    this.maximumTemperature = this.POWERSAVING_MAX_TEMPERATURE;
+    this.temperature = this.DEFAULT_TEMPERATURE;
+    this.powerSaving = true;
   }
 
   getCurrentTemperature(){
     return this.temperature;
   };
 
+  isMinimumTemperature(){
+    return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+
+  isMaximumTemperature(){
+    return this.temperature === this.maximumTemperature;
+  }
+
   up(){
+    if (this.isMaximumTemperature()) {
+      throw Error('Cannot get any hotter than this!');
+    }
     this.temperature ++;
   }
 
   down(){
+    if (this.isMinimumTemperature()) {
+      throw Error('Cannot get any colder than this!');
+    }
     this.temperature --;
+  }
+
+  togglePowerSavingOn(){
+    this.powerSaving = true;
+    this.setMaxTemperature();
+  }
+
+  togglePowerSavingOff(){
+    this.powerSaving = false;
+    this.setMaxTemperature();
+  }
+
+  setMaxTemperature(){
+    this.maximumTemperature = this.powerSaving ? this.POWERSAVING_MAX_TEMPERATURE : this.MAX_TEMPERATURE;
+  }
+
+  resetTemperature(){
+    this.temperature = this.DEFAULT_TEMPERATURE;
+  }
+
+  energyUsage() {
+    if (this.temperature <= this.LOW_ENERGY_USAGE) {
+      return 'low-usage';
+    } else if (this.temperature <= this.MEDIUM_ENERGY_USAGE && this.temperature > this.LOW_ENERGY_USAGE){
+      return 'medium-usage';
+    } else {
+      return 'high-usage';
+    }
   }
 
 }
